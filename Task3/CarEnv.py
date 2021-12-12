c8eb4d2b3c28b3edefa3b98e2cf65b32a8718ede
yuxiao98@uw.edu <yuxiao1155@gmail.com>
2021-12-12 13:43:30 -0600

    update specification

--- Task3/CarEnv.py
+++ Task3/CarEnv.py
@@ -268,11 +268,8 @@ def main():
         state = env.reset()
         ep_reward = 0
         while True:
-            # env.render()
             action = dqn.choose_action(state)
             next_state, reward , done= env.step(action)
-            # x, x_dot, theta, theta_dot = next_state
-            # reward = reward_func(env, x, x_dot, theta, theta_dot)
 
             dqn.store_transition(state, action, reward, next_state)
             ep_reward += reward
@@ -288,11 +285,8 @@ def main():
         r = copy.copy(reward)
         reward_list.append(r)
         ax.set_xlim(0,300)
-        #ax.cla()
         ax.plot(reward_list, 'g-', label='total_loss')
         plt.pause(0.001)
-
-
 
 if __name__ == '__main__':
     main()
